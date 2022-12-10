9484f0d425002469391a348e7cd224c49e34e702
Cla <clamattia@gmail.com>
2022-12-10 11:12:37 +0100

    day10

--- time_each.sh
+++ time_each.sh
@@ -2,7 +2,11 @@ AOC_BASE=./*/
 for EXE in $(find $AOC_BASE -executable -type f | sort)
 do
     INPUTA=$(echo $EXE | awk -F '/' '{print "./" $2 "/input"}')
-    perf stat $EXE ${INPUTA}>>output
+    PER=$(echo $EXE | awk -F '[/]' '{print "./" $2 $3 "_perf"}')
+    echo $PER
+    perf stat -o $PER $EXE ${INPUTA}>>output
+    cat $PER >> perf_stat
+    rm $PER
 done
 diff output expected_output && echo "PASS\n"; rm output
 
